aadf9d4a076e9ad2e59f559977240abf57619dd7
Shaquille Mandy <shaqmandy@gmail.com>
2022-10-05 12:10:00 -0700

    added sign-in page, added firebase to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/navigation/navigation.component';
 import Home from './components/routes/home/home.component';
+import SignIn from './components/routes/sign-in/sign-in.component';
 import './App.css';
 
 const Shop = () => {
@@ -16,6 +17,7 @@ function App() {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
       
     </Routes>
