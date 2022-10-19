627e7cf48b7b64dbade94a5914a097ee81637a99
Shaquille Mandy <shaqmandy@gmail.com>
2022-10-18 21:09:52 -0700

    added checkout page with checkout items and checkout cart functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from './components/navigation/navigation.component';
 import Home from './components/routes/home/home.component';
 import Authentication from './components/routes/authentication/authentication.component';
 import Shop from './components/routes/shop/shop.component';
+import Checkout from './components/routes/checkout/checkout.component';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
