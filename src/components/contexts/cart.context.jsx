dfe824a7087b02debc6cad4c2b84be6ebe9003bc
Shaquille Mandy <shaqmandy@gmail.com>
2022-11-03 09:37:41 -0700

    added styled components to a few components and prettified all the files

--- src/components/contexts/cart.context.jsx
+++ src/components/contexts/cart.context.jsx
@@ -1,104 +1,110 @@
 import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
-    // find if cartItems contains productToAdd
-    const existingCartItem = cartItems.find(
-        (cartItem) => cartItem.id === productToAdd.id
-    )
+  // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
 
-    // if found, increment quantity
-    if(existingCartItem) {
-        return cartItems.map((cartItem) =>
-            cartItem.id === productToAdd.id
-            // returning a new object so that react recognizes it has a new obj and re-renders
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-        )
-    }
+  // if found, increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? // returning a new object so that react recognizes it has a new obj and re-renders
+          { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
 
-    // return new array with modified cartItems / new cart item
-    return [...cartItems, { ...productToAdd, quantity: 1 }]
-}
+  // return new array with modified cartItems / new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 const remoteCartItem = (cartItems, cartItemToRemove) => {
-    // find the cart item to remove
-    const existingCartItem = cartItems.find(
-        (cartItem) => cartItem.id  === cartItemToRemove.id
-    )
-    
-    // check if the quantity === 1, if so, remove it from the cart
-    if(existingCartItem.quantity === 1) {
-        return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id)
-    }
+  // find the cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
 
-    // return back cartItems, with matching reduced quantity for that cartItem
-    return cartItems.map((cartItem) => 
-        cartItem.id === cartItemToRemove.id
-        // returning a new object so that react recognizes it has a new obj and re-renders
-        ? { ...cartItem, quantity:  cartItem.quantity - 1 }
-        : cartItem
-    )
-}
+  // check if the quantity === 1, if so, remove it from the cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // return back cartItems, with matching reduced quantity for that cartItem
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? // returning a new object so that react recognizes it has a new obj and re-renders
+        { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
 
 const clearCartItem = (cartItems, cartItemToClear) => {
-    // clear the item from the cart if it matches cartItemToClear
-    return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id )
-}
+  // clear the item from the cart if it matches cartItemToClear
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+};
 
 export const CartContext = createContext({
-    isCartOpen: false,
-    setIsCartOpen: () => {},
-    cartItems: [],
-    addItemToCart: () => {},
-    removeItemFromCart: () => {},
-    clearItemFromCart: () => {},
-    cartCount: 0,
-    cartTotal: 0
-})
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
+  cartCount: 0,
+  cartTotal: 0,
+});
 
 export const CartProvider = ({ children }) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItems] = useState([])
-    const [cartCount, setCartCount] = useState(0);
-    const [cartTotal, setCartTotal] = useState(0);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
-    
-    const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems, productToAdd))
-    }
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
 
-    const removeItemFromCart = (cartItemToRemove) => {
-        setCartItems(remoteCartItem(cartItems, cartItemToRemove))
-    }
+  const removeItemFromCart = (cartItemToRemove) => {
+    setCartItems(remoteCartItem(cartItems, cartItemToRemove));
+  };
 
-    const clearItemFromCart = (cartItemToRemove) => {
-        setCartItems(clearCartItem(cartItems, cartItemToRemove))
-    }
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+  };
 
-    // use effect for cartItems count, separating useEffects so that they control their own scopes
-    useEffect(() => {
-        const newCartCount = cartItems.reduce((accumulator, cartItem) => (
-            accumulator + cartItem.quantity 
-        ), 0)
+  // use effect for cartItems count, separating useEffects so that they control their own scopes
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (accumulator, cartItem) => accumulator + cartItem.quantity,
+      0
+    );
 
-        setCartCount(newCartCount);
-    }, [cartItems])
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
-    // use effect for cartTotal
-    useEffect(() => {
-        const newCartTotal = cartItems.reduce((accumulator, cartItem) => (
-            accumulator + ( cartItem.quantity * cartItem.price ) 
-        ), 0)
+  // use effect for cartTotal
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (accumulator, cartItem) =>
+        accumulator + cartItem.quantity * cartItem.price,
+      0
+    );
 
-        setCartTotal(newCartTotal);
-    }, [cartItems])
+    setCartTotal(newCartTotal);
+  }, [cartItems]);
 
-    const value = { 
-        isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart,
-         clearItemFromCart, cartItems, cartCount, cartTotal 
-    };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    removeItemFromCart,
+    clearItemFromCart,
+    cartItems,
+    cartCount,
+    cartTotal,
+  };
 
-    return (
-        <CartContext.Provider value={value}>{children}</CartContext.Provider>
-    )
-}
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,66 +1,69 @@
-import { Fragment, useContext } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Fragment, useContext } from "react";
+import { Outlet } from "react-router-dom";
 
-import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-import { UserContext } from '../contexts/user.context';
-import { CartContext } from '../contexts/cart.context';
+import { UserContext } from "../contexts/user.context";
+import { CartContext } from "../contexts/cart.context";
 
-import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import { NavigationContainer,  LogoContainer, NavLinks, NavLink } from './navigation.styles'; 
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from "./navigation.styles";
 
 const Navigation = () => {
-    /*useContext makes the componennt re-render when a value inside of its context updates
-    * By getting values from useContext, you have HOOKED your component into context
-    */
-    const { currentUser } = useContext(UserContext);
-    const { isCartOpen } = useContext(CartContext);
+  /*useContext makes the componennt re-render when a value inside of its context updates
+   * By getting values from useContext, you have HOOKED your component into context
+   */
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
-    return (
-        <Fragment>
-            {/* 
+  return (
+    <Fragment>
+      {/* 
                 A <Fragment /> is a component that gets rendered to nothing when it is mounted on the DOM
                 Useful if you don't want to render unnecessary html elements, like a outter useless <div></div>
             */}
-            <NavigationContainer>
-                <LogoContainer to='/'>
-                    {/* 
+      <NavigationContainer>
+        <LogoContainer to="/">
+          {/* 
                         Made this a ReactComponent with the import statment below 
                         import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-                    */}
-                    <CrwnLogo className='logo' />
-                </LogoContainer>
+            */}
+          <CrwnLogo className="logo" />
+        </LogoContainer>
 
-                <NavLinks>
-                    {/* A <Link /> is basically a <a> but it works well with <BrowserRouter /> */}
-                    <NavLink to='/shop'>
-                        SHOP
-                    </NavLink>
-                    
-                    {/* if the currentUser is not null, aka they have logged in
+        <NavLinks>
+          {/* A <Link /> is basically a <a> but it works well with <BrowserRouter /> */}
+          <NavLink to="/shop">SHOP</NavLink>
+
+          {/* if the currentUser is not null, aka they have logged in
                         then change the link element to a sign out span element
                         with this ternary statement below
                     */}
-                    { currentUser ? (
-                        // will render the component as a span and not a link, because you used the as keyword
-                        <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>
-                    ) : (
-                        <NavLink to='/auth'>
-                            SIGN IN
-                        </NavLink>
-                    )}
-                    <CartIcon />
-                </NavLinks>
-                {isCartOpen && <CartDropdown />}
-            </NavigationContainer >
-            
-            {/* A Outlet helps control whether or not something will render */}
-            <Outlet />
-        </Fragment>
-    )
-}
+          {currentUser ? (
+            // will render the component as a span and not a link, because you used the as keyword
+            <NavLink as="span" onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to="/auth">SIGN IN</NavLink>
+          )}
+          <CartIcon />
+        </NavLinks>
+        {isCartOpen && <CartDropdown />}
+      </NavigationContainer>
+
+      {/* A Outlet helps control whether or not something will render */}
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
